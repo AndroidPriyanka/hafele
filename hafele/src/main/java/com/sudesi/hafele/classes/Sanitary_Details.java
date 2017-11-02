d8d7efa11c411d07221dbad2a30f5f1a86ce1c73
aniketadsulsudesi <aniketadsul@sudesi.in>
2017-11-02 17:40:02 +0530

    Sanitary complaint details done by Aniket Adsul

---
+++ hafele/src/main/java/com/sudesi/hafele/classes/Sanitary_Details.java
@@ -0,0 +1,49 @@
+package com.sudesi.hafele.classes;
+
+/**
+ * Created by komal on 10/28/2017.
+ */
+
+public class Sanitary_Details {
+    //.........................For Sanitary Details............
+//public String Complaint_No;
+    public String radio_sanitary;
+    public String type_of_sanitary;
+    public String sanitary_product;
+    public String sanitary_leakage;
+    public String sanitary_type_of_leakage;
+    public String does_not_operate;
+    public String type_does_not_operate;
+    public String weak_flow;
+    public String type_of_weak_flow;
+    public String asthetics;
+    public String type_of_asthetics;
+    public String warranty;
+    public String noise;
+    public String flush_not_working;
+    public String type_of_flush_not_working;
+    public String drainage;
+    public String type_of_drainage;
+    public String LMD;
+    public String Complant_No;
+    public String Product_Category;
+    public String product_Sub_Category;
+    public String article_no;
+    public String Comment;
+    public String sync_status;
+    public String Insert_Date;
+    public String Result;
+    public String sparce_defect;
+    public String complete_set;
+    public String site_Issue_Reason;
+    public String Action;
+    public String wrong_product_reason;
+    public String Reason_For_Unresolved;
+    public String Closure_Status;
+    public String Closed_Date;
+    public String Updated_Date;
+    public String technician;
+    public String date;
+    public String gudance_given;
+
+}
